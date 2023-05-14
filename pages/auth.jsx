85b70f14f64ce5116274ed97cd85fc4dd4c444f3
Ayo <heyayomideadebisi@gmail.com>
2023-05-14 16:40:59 +0100

    add change page stuff

--- pages/auth.jsx
+++ pages/auth.jsx
@@ -8,13 +8,20 @@ const Auth = () => {
     const [variant, setVariant] = useState("signin")
     const [loadSpinner, setLoadSpinner] = useState(false)
     const [showPassword, setShowPassword] = useState(false)
+    const changePage = () => {
+        if(variant == "signin"){
+            setVariant("signup")
+        } else {
+            setVariant("signin")
+        }
+    }
 
     return (
         <div className="h-screen flex items-center px-8 justify-center bg-gradient-to-tr from-purple-600 to-purple-400">
             <div className="bg-white p-4 w-full sm:w-1/2 md:w-[25rem] rounded shadow-md ">
                 {variant == "signin" ? <h1 className="text-2xl mb-4 font-medium ">Sign in</h1> : <h1 className="text-2xl mb-4 font-medium ">Sign up</h1>}
                 <div>
-                    {variant == "signout" &&
+                    {variant == "signup" &&
                         <div className="flex relative">
 
                             <input type="text" placeholder="input name" className="border border-neutral-400 rounded px-2 w-full my-1 placeholder:text-sm outline-none focus:border-purple-600 py-2 text-sm" />
@@ -48,6 +55,7 @@ const Auth = () => {
                         <button className="cursor-pointer rounded w-full text-black border border-neutral-500 font-medium px-4 py-1 hover:text-white hover:bg-black flex items-center gap-2 justify-center">sign in with google <FcGoogle /></button>
                     </div>
                 </div>
+                <p className="text-xs mt-4 text-center">do not have an account? <span className="font-bold underline cursor-pointer" onClick={changePage}>sign up</span></p>
             </div>
         </div>
     )
