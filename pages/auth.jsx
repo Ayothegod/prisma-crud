90e2cf114e43c2778cb829b95837754f4f32449b
Ayo <heyayomideadebisi@gmail.com>
2023-05-14 15:48:10 +0100

    add auth base style and add prisma client

--- pages/auth.jsx
+++ pages/auth.jsx
@@ -1,11 +1,13 @@
 
 
-const auth = () => {
+const Auth = () => {
   return (
-    <div>
-        Hello to the auth page!!!!
+    <div className="h-screen flex items-center px-8 justify-center bg-gradient-to-tr from-purple-600 to-purple-400">
+        <div className="bg-white p-4 w-full sm:w-1/2 rounded shadow-md ">
+
+        </div>
     </div>
   )
 }
 
-export default auth
+export default Auth
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="h-full text-center space-y-2">
-        <h1 className='text-2xl '>Lets Cook</h1>
+        <h1 className='text-2xl '>Lets Cook, this dou, its amazing</h1>
               </main>
     </>
   );
