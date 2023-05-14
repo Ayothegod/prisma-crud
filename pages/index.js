d75e17438cb69155f1f65df67addb81aa1f76caa
Ayo <heyayomideadebisi@gmail.com>
2023-05-14 23:52:59 +0100

    add axios

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import { useRouter } from "next/router"
 import Link from 'next/link';
 
 
+
 export default function Home() {
   const router = useRouter()
   const { data: session, status } = useSession()
