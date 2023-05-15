24149500785301edbe9dd437db9f1359bd223154
Ayo <heyayomideadebisi@gmail.com>
2023-05-15 10:54:31 +0100

    why is my modal bg taking all height

--- components/MobileCreate.jsx
+++ components/MobileCreate.jsx
@@ -6,20 +6,18 @@ const MobileCreate = ({mobileCreate,setMobileCreate}) => {
   const {userData} = useUserContext()
   console.log(userData)
   return (
-    <div className="text-white">
-      <div className="flex items-center flex-col justify-center h-screen">
-      <div>
-        <div className="w-10 h-10 relative">
+    <div className="text-white h-screen">
+      <div className="flex items-center flex-col justify-center h-full bg-white">
 
+      <div className="flex items-center gap-4">
+
+        <div className="w-8 h-8 relative rounded-full overflow-hidden">
         <Image src={userData[0].image} alt={userData[0].name} fill className="absolute"/>
         </div>
+        <p className="font-semibold">@{userData[0].name}</p>
+
       </div>
-      {userData && 
-      <div className="flex flex-col">
-      <p className="p-4">{userData[0].name} bruh</p>
-      <p className="p-4">{userData[0].email} bruh</p>
-      </div>
-      }
+
       <input type="text" className="border" />
       <input type="text" className="border"/>
       <button onClick={() => setMobileCreate(!mobileCreate)}>Close modal</button>
