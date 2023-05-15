f44e538d4099633561c60bbad8a16e2b7b29bbaa
Ayo <heyayomideadebisi@gmail.com>
2023-05-15 10:43:15 +0100

    initial userData error solved

--- components/MobileCreate.jsx
+++ components/MobileCreate.jsx
@@ -1,15 +1,26 @@
 import { useUserContext } from "@/hooks/hooks"
+import Image from "next/image"
 
 
 const MobileCreate = ({mobileCreate,setMobileCreate}) => {
   const {userData} = useUserContext()
   console.log(userData)
   return (
-    <div>
-      Hello
+    <div className="text-white">
+      <div className="flex items-center justify-center h-screen">
+      <div>
+        <div className="w-10 h-10 relative">
+
+        {/* <Image src={userData?.Image} alt="hello" fill className="absolute"/> */}
+        </div>
+      </div>
+      {userData && 
+      <p className="p-4">{userData[0].name} bruh</p>
+      }
       <input type="text" className="border" />
       <input type="text" className="border"/>
       <button onClick={() => setMobileCreate(!mobileCreate)}>Close modal</button>
+      </div>
     </div>
   )
 }
