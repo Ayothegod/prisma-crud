d2b58599769c233599d7403007eadc016de40fd3
Ayo <heyayomideadebisi@gmail.com>
2023-05-16 11:41:19 +0100

    trying to use date to subtract from date

--- components/SinglePosts.jsx
+++ components/SinglePosts.jsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 import { compareAsc, format } from 'date-fns'
 
 const SinglePosts = ({ post }) => {
+    
     // console.log(post);
     return (
         <div className="bg-white shadow-lg p-4 flex gap-4">
@@ -13,7 +14,8 @@ const SinglePosts = ({ post }) => {
 
                 <div className="flex items-center w-full gap-2">
                     <p className="font-medium ">{post.user.name}</p>
-                    <p className="">{post.createdAt.slice(0,10)}</p>
+                    <p className="text-xs opacity-60">{post.createdAt.slice(0,10)}
+                    {format(new Date(2014, 1, 11), 'yyyy-MM-dd')}</p>
                 </div>
                 <div>
                     <p>{post.postData}</p>
