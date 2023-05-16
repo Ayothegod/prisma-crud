34964044b964eaef03e61dbbb84b237a9ee9840f
Ayo <heyayomideadebisi@gmail.com>
2023-05-16 19:31:50 +0100

    made a mini post hook

--- hooks/usePost.jsx
+++ hooks/usePost.jsx
@@ -1,13 +1,27 @@
 import { useState, useEffect } from "react"
 
-const usePost = () => {
-    
+const usePost = (body) => {
+    const [data,setData] = useState("")
+    const [loading,setLoading] = useState(true)
+    const [error,setError] = useState("")
 
-    const postData = async() => {
-        
+    const postData = async(body) => {
+        try {
+            const response = await fetch("/api/routes", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json"
+                },
+                body: JSON.stringify(body)
+            })
+            console.log(response);
+            setLoading(false)
+        } catch (error) {
+            setError(error.message)
+        }
     }
 
-    return {data,loading};
+    return {data,loading,error};
 }
 
 export default usePost
