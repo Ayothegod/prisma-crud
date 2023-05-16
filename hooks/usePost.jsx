6282bde1c66ef8a3f8b83a8581859eabbca85fcd
Ayo <heyayomideadebisi@gmail.com>
2023-05-16 19:24:05 +0100

    creating useFetch

---
+++ hooks/usePost.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react"
+
+const usePost = () => {
+    
+
+    const postData = async() => {
+        
+    }
+
+    return {data,loading};
+}
+
+export default usePost
