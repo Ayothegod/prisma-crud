03078542670d9e406ae576fae7f4ef875d22f307
Ayo <heyayomideadebisi@gmail.com>
2023-05-28 14:57:13 +0100

    deploy

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,21 @@ export default function Home() {
   
   const fetcher = (at) => axios.get(at)
   const {data,error,isLoading} = useSWR("/api/data",fetcher)
-  console.log(data,error)
+  // console.log(data.data,error)
   return (
-    <>
+    <div className='max-w-[30rem] mx-auto mt-20'>
     <p className="text-3xl text-blue-600">Hello</p>
-    </>
+    <section>
+      {
+        data && data.data.map(data => (
+          <div key={data.id} className='flex gap-4'>
+            <p className='font-bold text-lg'>{data.name}</p>
+            <p>{data.email}</p>
+          </div>
+        ))
+      }
+    </section>
+    </div>
   );
 }
 
