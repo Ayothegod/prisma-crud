a8616a092309c2424e155a39f917a8701253ce2c
Ayo <heyayomideadebisi@gmail.com>
2023-05-28 16:29:26 +0100

    change data source

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,9 +1,21 @@
-import prisma from "@/db/prisma"
+// import prisma from "@/db/prisma"
 
 export default async function handler(req, res) {
     try {
-        const data = await prisma.data.findMany()
-        // console.log(data);
+        // const data = await prisma.data.findMany()
+        const data = [
+            {
+            id:"1234uwyuwe78weu",
+            name:"Ayo",
+            email:"ayomie@gmail.com"
+        },
+            {
+            id:"1234uwyuksdjksdio89we78weu",
+            name:"Brother",
+            email:"Brother@gmail.com"
+        },
+    ]
+        console.log(data);
         res.status(200).json(data)
     } catch (error) {
         res.status(404).json(error.message)
