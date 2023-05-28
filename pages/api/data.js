f2a596ebc0f02c7a228395aebd0beaac6897cc1a
Ayo <heyayomideadebisi@gmail.com>
2023-05-28 14:41:26 +0100

    get api route

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,5 +1,11 @@
-// import prisma from "@/lib/prisma"
+import prisma from "@/db/prisma"
 
 export default async function handler(req, res) {
-    res.status(200).json({data:"hello user"})
+    try {
+        const data = await prisma.data.findMany()
+        console.log(data);
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(404).json(error.message)
+    }
 }
