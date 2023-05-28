675bcc19bee19fa92803b1cfe8b52cfecd489882
Ayo <heyayomideadebisi@gmail.com>
2023-05-28 14:51:14 +0100

    get first data

--- pages/api/data.js
+++ pages/api/data.js
@@ -3,7 +3,7 @@ import prisma from "@/db/prisma"
 export default async function handler(req, res) {
     try {
         const data = await prisma.data.findMany()
-        console.log(data);
+        // console.log(data);
         res.status(200).json(data)
     } catch (error) {
         res.status(404).json(error.message)
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 // import { signIn, useSession, signOut, getSession } from "next-auth/react"
 import Link from 'next/link';
 import axios from 'axios';
+import useSWR from "swr"
 
 export default function Home() {
   
-  
+  const fetcher = (at) => axios.get(at)
+  const {data,error,isLoading} = useSWR("/api/data",fetcher)
+  console.log(data,error)
   return (
     <>
     <p className="text-3xl text-blue-600">Hello</p>
