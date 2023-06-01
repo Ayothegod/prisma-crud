2ca825170c740d7d11bee580c46b9bea2328d222
Ayo <heyayomideadebisi@gmail.com>
2023-06-01 23:39:27 +0100

    daily commit

--- pages/api/data.js
+++ pages/api/data.js
@@ -2,7 +2,6 @@
 
 export default async function handler(req, res) {
     try {
-        // const data = await prisma.data.findMany()
         const data = [
             {
             id:"1234uwyuwe78weu",
@@ -15,7 +14,7 @@ export default async function handler(req, res) {
             email:"Brother@gmail.com"
         },
     ]
-        console.log(data);
+        // console.log(data);
         res.status(200).json(data)
     } catch (error) {
         res.status(404).json(error.message)
