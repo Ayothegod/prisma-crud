3b6d224842537871b377dedf16973d4255722947
Ayo <heyayomideadebisi@gmail.com>
2023-06-03 19:54:38 +0100

    to netlify

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,20 @@ import axios from 'axios';
 import useSWR from "swr"
 // import prisma from '@/db/prisma';
 
-export default function Home() {
-  // console.log({value});
+export default function Home({data}) {
+  console.log({data});
   
-  const fetcher = (at) => axios.get(at)
-  const {data,error,isLoading} = useSWR("/api/data",fetcher)
+  // const fetcher = (at) => axios.get(at)
+  // const {data,error,isLoading} = useSWR("/api/data",fetcher)
   return (
     <div className='max-w-[30rem] mx-auto mt-20'>
     <p className="text-3xl text-blue-600">Hello</p>
     <section>
-      {
+      {/* {
         error && "error fetching data"
-      }
+      } */}
       {
-        data && data.data.map(data => (
+        data && data.map(data => (
           <div key={data.id} className='flex gap-4'>
             <p className='font-bold text-lg'>{data.name}</p>
             <p>{data.email}</p>
@@ -30,11 +30,11 @@ export default function Home() {
   );
 }
 
-// export async function getStaticProps(){
-//   const data = await prisma.data.findMany()
-//   return {
-//     props: {
-//       value : JSON.parse(JSON.stringify(data))
-//     }
-//   }
-// }
+export async function getStaticProps(){
+  const data = await prisma.data.findMany()
+  return {
+    props: {
+      data : JSON.parse(JSON.stringify(data))
+    }
+  }
+}
