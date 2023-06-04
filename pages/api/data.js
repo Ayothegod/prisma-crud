a723d57f653fafc744621f00667730f4b8ec30f9
Ayo <heyayomideadebisi@gmail.com>
2023-06-04 13:38:26 +0100

    init

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,18 +1,8 @@
 import prisma from "@/db/prisma"
 
-// const Pool = require("pg").Pool
-// const pool = new Pool({
-//     user:"postgres",
-//     password:"Ayo1234$$Ayo",
-//     host:"db.ztoijqejifmeyrdwnjql.supabase.co",
-//     port:5432,
-//     database:"postgres"
-// })
-
 export default async function handler(req, res) {
     try {
         const data = await prisma.data.findMany()
-        // const data = await pool.query("SELECT * FROM data")
         console.log(data);
         res.status(200).json(data)
     } catch (error) {
--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,23 @@
 import Link from 'next/link';
 import axios from 'axios';
 import useSWR from "swr"
-import prisma from '@/db/prisma';
+// import prisma from '@/db/prisma';
 
-export default function Home({data}) {
-  console.log({data});
+export default function Home() {
+  // console.log({data});
   
-  // const fetcher = (at) => axios.get(at)
-  // const {data,error,isLoading} = useSWR("/api/data",fetcher)
+  const fetcher = (at) => axios.get(at)
+  const {data,error,isLoading} = useSWR("/api/data",fetcher)
+  console.log(data);
   return (
     <div className='max-w-[30rem] mx-auto mt-20'>
     <p className="text-3xl text-blue-600">Hello</p>
     <section>
-      {/* {
-        error && "error fetching data"
-      } */}
       {
-        data && data.map(data => (
+        error && "error fetching data"
+      }
+      {
+        data && data.data.map(data => (
           <div key={data.id} className='flex gap-4'>
             <p className='font-bold text-lg'>{data.name}</p>
             <p>{data.email}</p>
@@ -30,11 +31,11 @@ export default function Home({data}) {
   );
 }
 
-export async function getStaticProps(){
-  const data = await prisma.data.findMany()
-  return {
-    props: {
-      data : JSON.parse(JSON.stringify(data))
-    }
-  }
-}
+// export async function getStaticProps(){
+//   const data = await prisma.data.findMany()
+//   return {
+//     props: {
+//       data : JSON.parse(JSON.stringify(data))
+//     }
+//   }
+// }
